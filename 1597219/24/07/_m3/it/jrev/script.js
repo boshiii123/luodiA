488f2e2545665053651b00c28f83eeea8df752c4
boshiii123 <143775210+boshiii123@users.noreply.github.com>
2024-09-30 12:47:02 +0800

    Delete 1597219 directory

--- 1597219/24/07/_m3/it/jrev/script.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){function e(e){$("html, body").animate({scrollTop:e},1e3)}setTimeout(e($(document).height()),2e3),$(".btn").on("click",function(){$(this).closest(".step").hide().next(".step").fadeIn(),e(200)}),$(".option").on("click",function(){$(this).toggleClass("checked tr-selected")})})
