d1e1c29f5e89597d23e2bc58286ee52a8f545134
boshiii123 <143775210+boshiii123@users.noreply.github.com>
2024-10-09 22:30:50 +0800

    Add files via upload

---
+++ zdfy/cdn.jmp-assets.com/assets/2262/js/multilang_mainstream.js
@@ -0,0 +1,260 @@
+var langs = {
+    en: {
+        title: 'WARNING!',
+        mainText: 'These girls are looking for attention and affection.',
+        btnText: 'CONTINUE',
+        subtitle: 'YOU MUST FOLLOW THE RULES GIVEN BELOW',
+        item1: '💋 If you see someone you know, DO NOT publicize it. Do not spread rumors.',
+        item2: '💋 The women have requested that we not let those younger than 18 contact them because of past rude behavior by younger men.',
+        btnFinal: 'I AGREE',
+        adult: 'adult only',
+    },
+    pt: {
+        title: 'ALERTA!',
+        mainText: 'Muitas dessas garotas estão procurando por um homem e novas impressões.',
+        btnText: 'CONTINUAR',
+        subtitle: 'VOCÊ DEVE SEGUIR AS REGRAS ABAIXO',
+        item1: '💋 Caso você veja alguém que você conhece, NÃO o torne público. Não espalhe rumores.',
+        item2: '💋 As mulheres pediram que não deixássemos menores de 18 anos entrar em contacto com elas por causa de um comportamento rude do passado por homens mais jovens.',
+        btnFinal: 'EU CONCORDO',
+        adult: 'Apenas adultos',
+    },
+    cs: {
+        title: 'UPOZORNĚNÍ!',
+        mainText: 'Tyto ženy hledají pozornost a nové pocity.',
+        btnText: 'POKRAČOVAT',
+        subtitle: 'PLATÍ PRAVIDLA NÍŽE',
+        item1: '💋 Pokud vidíte někoho, koho znáte, NEZVĚŘEJŇUJTE to. Nerozšiřujte zvěsti.',
+        item2: '💋 Ženy požádaly, aby nám nedovolí osoby mladší 18 spojit se s nimi, protože v minulosti hrubé chování u mladších mužů.',
+        btnFinal: 'SOUHLASÍM',
+        adult: 'Pouze pro dospělé',
+    },
+    pl: {
+        title: 'OSTRZEŻENIE!',
+        mainText: 'Kobiety na tej stronie szukają uwagi i nowych doświadczeń.',
+        btnText: 'DALEJ',
+        subtitle: 'NALEŻY PRZESTRZEGAĆ PONIŻSZYCH ZASAD',
+        item1: '💋 Jeśli zobaczysz kogoś, kogo znasz, NIE publikuj tego. Nie rozpowszechniaj plotek.',
+        item2: '💋 Kobiety poprosiły, aby nie pozwalać osobom młodszym niż 18 lat kontaktować się z nimi z powodu niegrzecznych zachowań młodszych mężczyzn w przeszłości.',
+        btnFinal: 'ZGADZAM SIĘ',
+        adult: 'Tylko dla osób pełnoletnich',
+    },
+    fr: {
+        title: 'ATTENTION!',
+        mainText: 'Beaucoup de ces filles recherchent un homme et de nouvelles impressions.',
+        btnText: 'CONTINUEZ',
+        subtitle: 'VOUS DEVEZ SUIVRE LES RÈGLES CI-APRÈS',
+        item1: '💋 Si vous voyez quelqu\'un que vous connaissez, ne le publiez pas. Ne pas répandre des rumeurs.',
+        item2: "💋 Les femmes nous ont demandé de ne pas autoriser les moins de 18 ans à les contacter, parce qu'elles ont déjà subi le comportement grossier de certains hommes plus jeunes.",
+        btnFinal: 'JE SUIS D\'ACCORD',
+        adult: 'Réservé aux adultes'
+    },
+    ru: {
+        title: 'ПРЕДУПРЕЖДЕНИЕ',
+        mainText: 'Женщины на этом сайте ищут внимания и новых ощущений.',
+        btnText: 'Продолжить',
+        subtitle: 'ВЫ ДОЛЖНЫ СЛЕДОВАТЬ ПРАВИЛАМ НИЖЕ',
+        item1: '💋 Если вы увидите кого-то, кого знаете, НЕ разглашайте данную информацию. Не распространяйте слухи.',
+        item2: '💋 Наши пользовательницы просили оградить себя от внимания несовершеннолетних, в связи со случаями грубого обращения.',
+        btnFinal: 'ПРИНИМАЮ',
+        adult: 'Только для взрослых'
+    },
+    es: {
+        title: '¡ADVERTENCIA!',
+        mainText: 'Muchas de estas chicas buscan atención masculina y nuevas impresiones.',
+        btnText: 'CONTINUAR',
+        subtitle: 'DEBES CUMPLIR LAS NORMAS DE ABAJO',
+        item1: '💋 Si ves a alguien que conoces, NO lo publicites y no propagues rumores.',
+        item2: '💋 Debido a malas experiencias pasadas provocadas por el comportamiento de hombres de menos de 18 años, las mujeres nos han pedido que no dejemos que se pongan en contacto con ellas hombres jóvenes.',
+        btnFinal: 'ACEPTO',
+        adult: 'Solo para adultos'
+    },
+    it: {
+        title: 'ATTENZIONE!',
+        mainText: 'Molte di queste ragazze stanno cercando un uomo e nuove impressioni.',
+        btnText: 'CONTINUA',
+        subtitle: 'DEVI SEGUIRE LE REGOLE INDICATE QUI SOTTO',
+        item1: '💋 Se vedi qualcuno che conosci, NON renderlo pubblico. Non diffondere pettegolezzi.',
+        item2: '💋 Le donne ci hanno chiesto di non permettere a persone di età inferiore ai 18 anni di contattarle a causa del comportamento maleducato di alcuni giovani uomini in passato.',
+        btnFinal: 'ACCETTO',
+        adult: 'Solo per adulti'
+    },
+    uk: {
+        title: 'ПОПЕРЕДЖЕННЯ!',
+        mainText: 'Жінки на цьому сайті шукають уваги та нових почуттів.',
+        btnText: 'ПРОДОВЖИТИ',
+        subtitle: 'ВИ ПОВИННІ ДОТРИМУВАТИСЬ ПРАВИЛ НИЖЧЕ',
+        item1: '💋 Якщо ви побачите когось, кого знаєте, НЕ розголошуйте цю інформацію. Не поширюйте чутки.',
+        item2: '💋 Ви маєте бути повнолітнім. Наші юзери не бажають уваги неповнолітніх, через попередні випадки некоректної поведінки. ',
+        btnFinal: 'ПРИЙМАЮ',
+        adult: 'Тільки для дорослих '
+    },
+    de: {
+        title: 'WARNUNG!',
+        mainText: 'Viele dieser Mädchen suchen einen Mann und neue Eindrücke.',
+        btnText: 'FORTSETZEN',
+        subtitle: 'SIE MÜSSEN DIE UNTEN ANGEGEBENEN REGELN BEFOLGEN',
+        item1: '💋 Wenn Sie jemanden sehen, den Sie kennen, machen Sie echt NICHT öffentlich bekannt. Verbreiten Sie keine Gerüchte.',
+        item2: '💋 Die Frauen haben darum gebeten, dass wir nicht zulassen, dass Personen unter 18 Jahren Kontakt mit ihnen aufnehmen, weil sich jüngere Männer in der Vergangenheit unhöflich verhalten haben.',
+        btnFinal: 'ICH STIMME ZU',
+        adult: 'Nur für Erwachsene'
+    },
+    nl: {
+        title: 'Waarschuwing!',
+        mainText: 'Veel van deze meisjes zijn op zoek naar een man en nieuwe indrukken.',
+        btnText: 'Doorgaan',
+        subtitle: 'Je moet onderstaande regels volgen',
+        item1: '💋 Als je iemand ziet die je kent, publiceer deze dan NIET. Verspreid geen geruchten.',
+        item2: '💋 De vrouwen hebben verzocht dat we jongeren onder de 18 geen contact met hen laten opnemen vanwege onbeleefd gedrag van jongere mannen in het verleden.',
+        btnFinal: 'Ik ga akkoord',
+        adult: 'Alleen volwassenen'
+    },
+    zh: {
+        title: '警告！',
+        mainText: ' 他们可能是您的邻居或您认识的人。 您是否同意对这些妇女的身份保密？',
+        btnText: '继续',
+        subtitle: '您必须遵循以下规则',
+        item1: '💋 如果您看到认识的人，切勿公开。切勿散布流言。',
+        item2: '💋 女性要求我们不要让 18 岁以下的年轻人与她们联系，因为这样的年轻人以往有过一些粗鲁行为。',
+        btnFinal: '我同意',
+        adult: '仅限成人'
+    },
+    ar: {
+        title: 'تحذير!',
+        mainText: 'هل توافق على الحفاظ على السرية التامة فيما يتعلق بهوية هؤلاء النساء؟',
+        btnText: 'استمر',
+        subtitle: 'يجب أن تتبع القواعد الموضحة أدناه',
+        item1: '💋 إذا رأيت شخصًا تعرفه، فلا تنشر ذلك ولا تنشر الشائعات.',
+        item2: '💋 لقد طلبت النساء ألا نسمح لمن هم دون سن 18 بالاتصال بهم بسبب السلوك الوقح الذي قام به الرجال الأصغر سنًا في السابق.',
+        btnFinal: 'أوافق',
+        adult: 'للكبار فقط'
+    },
+    tr: {
+        title: 'UYARI!',
+        mainText: 'Bu kızlar ilgi ve samimiyet ararken ilk adımı atmaktan çekinmiyor.',
+        btnText: 'DEVAM',
+        subtitle: 'AŞAĞIDA VERİLEN KURALLARA UYMALISINIZ',
+        item1: '💋 Eğer tanıdığınız birini görürseniz, herkese AÇIKLAMAYIN. Dedikodu yapmayın.',
+        item2: '💋 Genç erkeklerin geçmişteki kötü davranışlarından dolayı kadınlar 18 yaşından küçük kişilerin kendileriyle iletişime geçmelerine izin vermememizi talep ettiler.',
+        btnFinal: 'KABUL EDİYORUM',
+        adult: 'Yalnızca yetişkinler'
+    },
+    ro: {
+        title: 'AVERTISMENT!',
+        mainText: 'Acestea pot fi printre vecinii tăi sau persoane pe care le cunoașteți. Sunteți de acord să păstreze discreția cu privire la identitatea acestor femei?',
+        btnText: 'CONTINUĂ',
+        subtitle: 'TREBUIE SĂ RESPECȚI REGULILE PREZENTATE MAI JOS',
+        item1: '💋 Dacă vei vedea pe cineva cunoscut, NU face acest lucru public. Nu răspândi zvonuri.',
+        item2: '💋 Femeile au cerut ca să nu ne lăsa pe cei mai tineri de 18 ani contactul ei din cauza comportamentului grosolan rămas singur cu bărbați mai tineri.',
+        btnFinal: 'SUNT DE ACORD',
+        adult: 'Destinat exclusiv adulților'
+    },
+    hr: {
+        title: 'UPOZORENJE!',
+        mainText: 'Ove žene traže pažnju i nove osjećaje.',
+        btnText: 'NASTAVI',
+        subtitle: 'MORATE POŠTOVATI DONJA PRAVILA',
+        item1: '💋 Ako vidite nekoga poznatog, NEMOJTE to objavljivati. Nemojte širiti glasine.',
+        item2: '💋 Žene su zatražili da se ne dopusti one mlađe od 18 ih kontaktirati zbog posljednjih nepristojnog ponašanja mlađih muškaraca.',
+        btnFinal: 'SLAŽEM SE',
+        adult: 'Samo za odrasle'
+    },
+    bg: {
+        title: 'ВНИМАНИЕ!',
+        mainText: 'Тези жени търсят внимание и нови чувства.',
+        btnText: 'ПРОДЪЛЖИ',
+        subtitle: 'ТРЯБВА ДА СЛЕДВАТЕ ПРАВИЛАТА, ПОСОЧЕНИ ПО-ДОЛУ',
+        item1: '💋 Ако видите някой, когото познавате, НЕ ГО публикувайте. Не разпространявайте слуха.',
+        item2: '💋 Трябва да сте навършили 18 години, за да влезете в този сайт.',
+        btnFinal: 'СЪГЛАСЯВАМ СЕ',
+        adult: 'Само за възрастни'
+    },
+    hu: {
+        title: 'FIGYELMEZTETÉS!',
+        mainText: 'Ezek a lányok figyelmet és szeretetet keresnek.',
+        btnText: 'FOLYTATÁS',
+        subtitle: 'KÖVETNED KELL AZ ALÁBBI SZABÁLYOKAT',
+        item1: '💋 Ha olyat látsz, akit ismersz, NE tedd közzé! Ne terjessz pletykákat.',
+        item2: '💋 A nők azt kérte, hogy ne hagyja, hogy azok a 18 évnél fiatalabb kapcsolati őket, mert már durva viselkedés fiatalabb férfiak.',
+        btnFinal: 'ELFOGADOM',
+        adult: 'Csak felnőttek'
+    },
+    sv: {
+        title: 'VARNING!',
+        mainText: 'Dessa tjejer söker uppmärksamhet och ömhet.',
+        btnText: 'Nästa',
+        subtitle: 'Du måste följa reglerna som anges nedan',
+        item1: '💋 Om du ser någon du känner, INTE offentliggör det. Inte spred rykten.',
+        item2: '💋 Tjejer har specifika krav på att vi inte ska tillåta personer under 18 år att ta kontakt med dem eftersom det tidigare förekommit oacceptabla beteenden gentemot tjejerna från yngre män.',
+        btnFinal: 'Fortsätt',
+        adult: 'only 18+'
+    },
+    no: {
+        title: 'OBS!',
+        mainText: 'Mange av disse jentene leter etter en mann og et nytt inntrykk.',
+        btnText: 'FORTSETT',
+        subtitle: 'DU MÅ FØLGE REGLENE SOM ER OPPFØRT NEDENFOR',
+        item1: '💋 Hvis du ser noen du kjenner, IKKE publisere det. Ikke spre rykter.',
+        item2: '💋 Du må være minst 18 år for å komme inn på dette nettstedet.',
+        btnFinal: 'JEG AKSEPTERER',
+        adult: 'KUN FOR VOKSNE'
+    },
+    fi: {
+        title: 'VAROITUS!',
+        mainText: 'Nämä tytöt etsivät huomiota ja hellyyttä.',
+        btnText: 'JATKA',
+        subtitle: 'Sinun tulee noudattaa alla annettuja SÄÄNTÖJÄ:',
+        item1: '💋 Jos näet jonkun tuntemasi henkilö, ÄLÄ tee asiasta julkista. Älä levitä juoruja.',
+        item2: '💋 Sinun on oltava vähintään 18 -vuotias päästäksesi tälle sivustolle.',
+        btnFinal: 'HYVÄKSYN',
+        adult: 'adult only'
+    },
+    da: {
+        title: 'ADVARSEL!',
+        mainText: 'Disse piger søger opmærksomhed og ømhed.',
+        btnText: 'FORTSÆT',
+        subtitle: 'DU SKAL FØLGE DE REGLER, DER ER ANGIVET NEDENFOR',
+        item1: '💋 Hvis du ser nogen du kender, DU MÅ IKKE offentliggøre det. Du må ikke sprede rygter',
+        item2: '💋 Kvinderne har bedt om, at vi ikke lader dem, der er yngre end 18, kontakte dem på grund af tidligere uhøflige opførsel udvist af yngre mænd.',
+        btnFinal: 'JEG ACCEPTERER',
+        adult: 'adult only'
+    },
+    ja: {
+        title: '警告!',
+        mainText: '私たちのサイトで真剣な交際を望む女性と出会い、素敵な時間を過ごすことを望んでいますか？',
+        btnText: '続行',
+        subtitle: '当サイトのガイドラインをご一読ください',
+        item1: '💋 あなたが知っている人を見たら、それを公表しないこと。噂を広めないこと。',
+        item2: '💋 このサイトにアクセスするには、18歳以上である必要があります。',
+        btnFinal: '同意します',
+        adult: 'adult only'
+    },
+    ko: {
+        title: '주목!',
+        mainText: '이 여성들은 관심과 새로운 감정을 찾고 있습니다.',
+        btnText: 'OK',
+        subtitle: '안내사항 참고',
+        item1: '💋 여성 회원분들께 예의를 지켜주세요. 한번에 두 명이상의 인연과 데이트하지 마시길 바랍니다',
+        item2: '💋 이 사이트에 액세스하려면 18세 이상이어야 합니다.',
+        btnFinal: '계속하기',
+        adult: 'adult only'
+    }
+};
+$(function() {
+    var langBrowser = (((navigator.language || navigator.languages) || '').split('-') || [])[0],
+        param = langs[langBrowser];
+    if (param == undefined) {
+        param = langs['en'];
+        // 'en' - default language, if you want change language, please select in object abbreviation. Example: 'fr', 'ru' etc.
+    } else {
+        $('.title').text(param['title']);
+        $('.main-text').text(param['mainText']);
+        $('.next').text(param['btnText']);
+        $('.btn-fin').text(param['btnFinal']);
+        $('.subtitle').text(param['subtitle']);
+        $('.item-1').text(param['item1']);
+        $('.item-2').text(param['item2']);
+        $('.item-3').text(param['item3']);
+        $('.adult').text(param['adult']);
+    }
+});
